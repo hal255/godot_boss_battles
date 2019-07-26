1a2fc2a824407d03b936244c0720d9db37af1e90
hal255 <hluu114@gmail.com>
2019-07-25 23:28:48 -0700

    Issue #1: Rotated Player on Movement

--- gd_scripts/player_movement.gd
+++ gd_scripts/player_movement.gd
@@ -4,8 +4,12 @@ var motion = Vector2()
 var speed = 300
 var slide_multiplier = 3
 
+var h_direction = 0
+var v_direction = 0
 
 func _physics_process(delta):
+	flip_player_image()
+	
 	motion.x = 0
 	motion.y = 0
 	
@@ -26,3 +30,19 @@ func _physics_process(delta):
 	
 	move_and_slide(motion)
 	pass
+	
+func flip_player_image():
+	# flip horizontal
+	if Input.is_key_pressed(KEY_LEFT) or Input.is_key_pressed(KEY_A):
+		h_direction = 0
+	elif Input.is_key_pressed(KEY_RIGHT) or Input.is_key_pressed(KEY_D):
+		h_direction = 1
+	
+	# flip vertical
+	if Input.is_key_pressed(KEY_UP) or Input.is_key_pressed(KEY_W):
+		v_direction = 1
+	elif Input.is_key_pressed(KEY_DOWN) or Input.is_key_pressed(KEY_S):
+		v_direction = 0
+
+	get_node("Sprite").set_flip_h(h_direction)
+	get_node("Sprite").set_flip_v(v_direction)
